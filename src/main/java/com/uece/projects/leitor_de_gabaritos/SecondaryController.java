981f135da8eab27a02085c6f763de0f0cab620e6
Artur de Maria <arturdemaria7@gmail.com>
2025-11-05 18:32:00 -0300

    refactor/folder-scheme: moving /src folder to project root

---
+++ src/main/java/com/uece/projects/leitor_de_gabaritos/SecondaryController.java
@@ -0,0 +1,12 @@
+package com.uece.projects.leitor_de_gabaritos;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
